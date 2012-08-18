f74e76f49622d41fe7b18aaf9c87d35487e83458
KamillaaaH <holanda.kamilla@gmail.com>
2012-08-18 17:37:19 -0300

    module C in new folder

---
+++ src/moduleVectorHash/hashset.c
@@ -0,0 +1,107 @@
+/*
+ * File:   hashset.c
+ * Author: kamilla
+ *
+ * Created on August 12, 2012, 5:33 PM
+ */
+
+#include "hashset.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * Function:  HashSetNew
+ * ---------------------
+ **/
+void HashSetNew(hashset *h, int elemSize, int numBuckets,
+        HashSetCompareFunction comparefn, HashSetFreeFunction freefn) {
+    /** An assert is raised unless all of the following conditions are met:
+     *    - elemSize is greater than 0.
+     *    - numBuckets is greater than 0.
+     *    - hashfn is non-NULL
+     *    - comparefn is non-NULL
+     **/
+    assert(h != NULL);
+    assert(elemSize > 0);
+    assert(numBuckets > 0);
+    assert(comparefn != NULL);
+    //initialization of vector;
+    vector v;
+    //now v->initialAllocation will be equal of numBuckets
+    VectorNew(&v, elemSize, (VectorFreeFunction) freefn, numBuckets);
+
+    //initialization of hash
+    h->v = v;
+    h->compareFunction = comparefn;
+    h->_free = freefn;
+
+    //initialization of vector
+    int i = 0;
+    for (i = 0; i <= numBuckets; i++) {
+        void *initValues = calloc(i, h->v.elemSize);
+        VectorAppend(&h->v, initValues);
+    }
+}
+
+/**
+ * Function: HashSetDispose
+ * ------------------------
+ * Disposes of any resources acquired during the lifetime of the
+ * hashset.
+ */
+void HashSetDispose(hashset *h) {
+    assert(h != NULL);
+    //cleaning vector
+    VectorDispose(&h->v);
+
+}
+
+/**
+ * Function: HashSetCount
+ * ----------------------
+ * Returns the number of elements residing in
+ * the specified hashset.
+ */
+int HashSetCount(const hashset *h) {
+    /** An assert is raised if hashset is null
+     */
+    assert(h != NULL);
+    //using VectorLength to return the number of elems
+    return VectorLength(&h->v);
+}
+
+void HashSetMap(hashset *h, HashSetMapFunction mapfn, void *auxData) {
+    /** An assert is raised if hashset is null
+     */
+    assert(h != NULL);
+    assert(mapfn !=NULL);
+    //sending mapfn to VectorMap
+    VectorMap(&h->v, (VectorMapFunction) mapfn, auxData);
+}
+
+/**
+ * Function: HashSetEnter
+ * ----------------------
+ * Inserts the specified element into the specified
+ * hashset.
+ */
+void HashSetEnter(hashset *h, const void *elemAddr, int position) {
+    //printf("%s", elemAddr);
+    
+    assert(h != NULL);
+    assert(elemAddr != NULL);
+
+    /* An assert is raised if the specified address is NULL, or
+     * if the embedded hash function somehow computes a hash code
+     * for the element that is out of the [0, numBuckets) range.
+     */
+    assert(-1 < position && position < h->v.initialAllocation);
+
+    //Replacing the value in the specified address
+    VectorReplace(&h->v, elemAddr, position);
+}
+
+
+
---
+++ src/moduleVectorHash/support.c
@@ -0,0 +1,88 @@
+/*
+ * File:   support.c
+ * Author: kamilla
+ *
+ * Created on August 12, 2012, 5:33 PM
+ */
+
+#include "hashset.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <assert.h>
+#include <string.h>
+
+/** Support functions */
+
+hashset counts;
+vector sortedCounts;
+
+struct frequency {
+    char s; // a particular string
+    int occurrences; // the number of times it occurs
+};
+
+/*
+ * Function: CompareLetter
+ * ----------------------
+ * Class of function designed to compare two elements, each identified
+ * by address.  The HashSetCompareFunction compares these elements and
+ * decides whether or not they are logically equal or or.
+ */
+static int CompareLetter(const void *elem1, const void *elem2) {
+    struct frequency *freq1 = (struct frequency *) elem1;
+    struct frequency *freq2 = (struct frequency *) elem2;
+    return (freq1->s - freq2->s);
+}
+
+/**
+ * Function: AddFrequency
+ * ----------------------
+ * Mapping function used to take a frequency stored
+ * in a hashset and append it to an vector of frequencies.
+ * The address of the vector is passed in as the auxiliarydata.
+ */
+
+static void AddFrequency(void *elem, void *v) {
+    VectorAppend((vector *) v, elem);
+}
+
+/**
+ * Function: PrintFrequency
+ * -------------------------
+ * Mapping function used to print one frequency stored in a hashset.  The
+ * file is passed as the client data, so that it can be used to print to
+ * any FILE *.
+ */
+
+static void PrintFrequency(void *elem, void *fp) {
+    struct frequency *freq = (struct frequency *) elem;
+    fprintf((FILE *) fp, "Data: %d\n",
+            freq->s);
+}
+
+
+static void PrintString(void *elemAddr, void *auxData) {
+    fprintf(stdout, "\n%s", (char*) elemAddr);
+}
+
+
+
+void Py_HashSetNew(int elemSize, int numBuckets) {
+    HashSetNew(&counts, elemSize, numBuckets, CompareLetter, NULL);
+}
+
+void Py_HashSetEnter(const void *itemAddr, int position) {
+    HashSetEnter(&counts, itemAddr, position);
+
+}
+
+void Py_PrintFn() {
+    fprintf(stdout, "\nHere is the contents of the table:\n");
+    HashSetMap(&counts, PrintString, stdout); // print contents of table
+}
+
+void Py_HashSetDispose() {
+    HashSetDispose(&counts);
+}
